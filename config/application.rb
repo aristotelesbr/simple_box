c1c7e8f9b8859947ab55e814c8e5d0a43c66f4af
Aristoteles Coutinho <aristotelesbr@coutinho.com>
2018-03-05 22:38:39 -0300

    removing minitest and improving testdirectories

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,13 @@
 require_relative 'boot'
 
-require 'rails/all'
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+require "action_cable/engine"
+require "sprockets/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -8,7 +15,8 @@ Bundler.require(*Rails.groups)
 
 module SimpleBox
   class Application < Rails::Application
-  	config.autoload_paths << Rails.root.join("lib")
+  	# Custom directories with classes and modules you want to be autoloadable.
+  	config.autoload_paths << Rails.root.join('lib')
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
